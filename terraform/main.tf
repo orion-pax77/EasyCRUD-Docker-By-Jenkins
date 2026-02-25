d087eac6b10f9ec853366629f5a807ab1b2480e2
Faizan Mansuri <faizanahmedmansuri@gmail.com>
2026-02-25 07:00:04 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -52,17 +52,30 @@ resource "aws_db_subnet_group" "rds_subnet" {
 resource "aws_db_instance" "mariadb" {
   identifier              = "easycrud-mariadb"
   allocated_storage       = 20
+  max_allocated_storage   = 20
+
   engine                  = "mariadb"
-  engine_version          = "default"
-  instance_class          = "db.t4g.micro"
+  engine_version          = "10.6.14"   # Free-tier supported version
+
+  instance_class          = "db.t3.micro"
+
   db_name                 = "easycruddb"
   username                = "admin"
   password                = "redhat123"
+
+  storage_type            = "gp2"
+
   db_subnet_group_name    = aws_db_subnet_group.rds_subnet.name
   vpc_security_group_ids  = [aws_security_group.rds_sg.id]
-  skip_final_snapshot     = true
+
   publicly_accessible     = true
   multi_az                = false
+  skip_final_snapshot     = true
+  deletion_protection     = false
+
+  backup_retention_period = 0
+  performance_insights_enabled = false
+  auto_minor_version_upgrade    = true
 
   tags = {
     Name = "EasyCRUD-mariadb"
